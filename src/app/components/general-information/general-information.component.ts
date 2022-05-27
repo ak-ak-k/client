7f9550ad7e2375c3732dda2248ee5388f5ffc0a6
ak-ak-k <ak.adityakirtane.git@gmail.com>
2022-05-27 12:56:44 +0530

    main

--- src/app/components/general-information/general-information.component.ts
+++ src/app/components/general-information/general-information.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-general-information',
   templateUrl: './general-information.component.html',
-  styleUrls: ['./general-information.component.scss']
+  styleUrls: ['./general-information.component.scss'],
 })
 export class GeneralInformationComponent implements OnInit {
+  show = false;
 
-  constructor() { }
+  show1 = false;
+  show2 = false;
+  show3 = false;
+  show4 = false;
+  show5 = false;
+  show6 = false;
+  show7 = false;
+  show8 = false;
+  show9 = false;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
