eb0405a72316d3644754bf4dc955e6c7ac26d50a
ADITYA KIRTANE <adityakirtane27@gmail.com>
2022-07-12 16:15:28 +0530

    aditya

--- src/app/contactssync/contactbottomsheet/contactbottomsheet.component.ts
+++ src/app/contactssync/contactbottomsheet/contactbottomsheet.component.ts
@@ -1,4 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { ContactsyncComponent } from '../contactsync/contactsync.component';
 
@@ -9,12 +10,31 @@ import { ContactsyncComponent } from '../contactsync/contactsync.component';
 })
 export class ContactbottomsheetComponent implements OnInit {
   constructor(
-    private _bottomSheetRef: MatBottomSheetRef<ContactsyncComponent>
-  ) {}
+    private _bottomSheetRef: MatBottomSheetRef<ContactsyncComponent>,
+    private fb: FormBuilder
+  ) {
+    this.form = fb.group({
+      name: this.fb.control('', [Validators.required]),
+      number: this.fb.control('', [
+        Validators.required,
+        Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$'),
+      ]),
+    });
+  }
 
   ngOnInit(): void {}
 
+  form: FormGroup = new FormGroup({});
+
+  get f() {
+    return this.form.controls;
+  }
+
   close() {
     this._bottomSheetRef.dismiss();
+  }
+
+  submit() {
+    console.log(this.form.value);
   }
 }
