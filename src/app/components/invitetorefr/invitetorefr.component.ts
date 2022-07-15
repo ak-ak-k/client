027797b1dc1944062d6fbe776d4942e6689c645a
ADITYA KIRTANE <adityakirtane27@gmail.com>
2022-07-15 20:51:52 +0530

    updated

--- src/app/components/invitetorefr/invitetorefr.component.ts
+++ src/app/components/invitetorefr/invitetorefr.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { InvitecontactsComponent } from '../invitecontacts/invitecontacts.component';
+import { RefrnowinviteComponent } from '../invitecontacts/refrnowinvite/refrnowinvite.component';
 
 @Component({
   selector: 'app-invitetorefr',
   templateUrl: './invitetorefr.component.html',
-  styleUrls: ['./invitetorefr.component.scss']
+  styleUrls: ['./invitetorefr.component.scss'],
 })
 export class InvitetorefrComponent implements OnInit {
+  constructor(private _bottomSheet: MatBottomSheet) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  refrnow() {
+    this._bottomSheet.open(RefrnowinviteComponent, {
+      panelClass: 'custom_width',
+    });
   }
 
+  invite() {
+    this._bottomSheet.open(InvitecontactsComponent);
+  }
 }
