807aa17c6782b1e1e114e4838ae08047a0481440
ADITYA KIRTANE <adityakirtane27@gmail.com>
2022-07-16 14:33:19 +0530

    update

--- src/app/components/invitecontacts/invitecontacts.component.ts
+++ src/app/components/invitecontacts/invitecontacts.component.ts
@@ -3,13 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-invitecontacts',
   templateUrl: './invitecontacts.component.html',
-  styleUrls: ['./invitecontacts.component.scss']
+  styleUrls: ['./invitecontacts.component.scss'],
 })
 export class InvitecontactsComponent implements OnInit {
+  invitecontact: Array<any> = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.invitecontact.push(
+      {
+        img: 'a',
+        name: 'aditya kirtane',
+        number: '8879751140',
+      },
+      {
+        img: 'd',
+        name: 'Dipesh Bhoir',
+        number: '8879751140',
+      },
+      {
+        img: 'r',
+        name: 'Roshan',
+        number: '8879751140',
+      },
+      {
+        img: 'h',
+        name: 'hardik karnavat',
+        number: '8879751140',
+      },
+      {
+        img: 's',
+        name: 'Shraddha',
+        number: '8879751140',
+      },
+      {
+        img: 's',
+        name: 'Simran',
+        number: '8879751140',
+      }
+    );
   }
 
+  ngOnInit(): void {}
 }
--- src/app/components/invitetorefr/invitetorefr.component.ts
+++ src/app/components/invitetorefr/invitetorefr.component.ts
@@ -11,12 +11,47 @@ import { RefrnowinviteComponent } from '../invitecontacts/refrnowinvite/refrnowi
 export class InvitetorefrComponent implements OnInit {
   name: string = 'aditya';
 
+  contacts: Array<any> = [];
+
   constructor(private _bottomSheet: MatBottomSheet) {
     this.name = this.name.charAt(0).toUpperCase() + this.name.slice(1);
     console.log(this.name);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.contacts.push(
+      {
+        img: 'a',
+        name: 'aditya kirtane',
+        number: '8879751140',
+      },
+      {
+        img: 'd',
+        name: 'Dipesh Bhoir',
+        number: '8879751140',
+      },
+      {
+        img: 'r',
+        name: 'Roshan',
+        number: '8879751140',
+      },
+      {
+        img: 'h',
+        name: 'hardik karnavat',
+        number: '8879751140',
+      },
+      {
+        img: 's',
+        name: 'Shraddha',
+        number: '8879751140',
+      },
+      {
+        img: 's',
+        name: 'Simran',
+        number: '8879751140',
+      }
+    );
+  }
 
   refrnow() {
     this._bottomSheet.open(RefrnowinviteComponent, {
